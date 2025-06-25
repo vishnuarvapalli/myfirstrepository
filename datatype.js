7c3743e3beae353786246757f8d74fdaba3300ce
vishnuarvapalli <vishnuarvapalli5@gmail.com>
2025-06-25 09:55:29 +0530

    commit all files

---
+++ datatype.js
@@ -0,0 +1,29 @@
+let number = 42;
+let string = "Hello, World!"; 
+let boolean = true;
+let underfineVar;
+let nullVar = null;
+let symbolVar = Symbol("unique");
+let bigIntVar = BigInt(123456789012345678901234567890);
+let objectVar = { key: "value"};
+let arrayVar =[1, 2, 3, 4, 5];
+let functionVar = function() {return "I am a function";}
+
+let object2={
+    name: "John Doe",
+    age:30,
+    isEmployed:true,
+    hobbies:["reading", "gaming", "coding"],
+}
+console.log("Data Types in JavaScript")
+console.log("Number:", number);
+console.log("String:", string)
+console.log("Boolean:", boolean);
+console.log("Undefined:", undefinedVar);
+console.log("Null:", nullVar);
+console.log("Symbol:", symbolVar);
+console.log("BigInt:", bigIntVar);
+console.log("Object:", objectVar);
+console.log("Array:", arrayVar);
+console.log("Function:", functionVar);
+console.log(object2);
---
+++ datatype1.js
@@ -0,0 +1,28 @@
+ let number = 42;
+ let string = "Hello World";
+ let boolean = true;
+ let undefinedVar;
+ let nullVar = null;
+ let symbolVar = Symbol("unique");
+ let bigIntVar = BigInt(123456789987456123454444875);
+ let objectVar = {key: "value"};
+ let arrayVar = [1,2,3,4,5];
+ let functionVar = function(){return "I am a function";}
+ let object2={
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    hobbies: ["reading","gaming","coding"],
+ }
+ console.log("Datatypes in JavaScript:");
+ console.log("Number:", number);
+ console.log("String:", string);
+ console.log("Boolean:", boolean);
+ console.log("Undefined:", undefinedVar);
+ console.log("Null:", nullVar);
+ console.log("Symbol:", symbolVar);
+ console.log("BigInt:", bigIntVar);
+ console.log("Object:", objectVar);
+ console.log("Array:", arrayVar);
+ console.log("Function:", functionVar);
+ console.log(object2);
